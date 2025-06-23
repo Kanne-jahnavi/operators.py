f7eae2d47f9de360db899c6cb2b512d2c2f4116c
Kanne-jahnavi <jahnavi12.27@gmail.com>
2025-06-23 20:01:35 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,22 @@
+#arthmetic operators
+a=10
+b=12
+c=a+b
+print(c)
+#assignment operators
+a=10
+b=12
+c=a==b
+print(c)
+#comparision operators
+a=12
+b=27
+print(a>b)
+#logical operators
+x="apple is a fruit"
+y="apple is  a fruit"
+c=x and y
+print(c)
+#bitwise operators
+a=12>>1
+print(a)
